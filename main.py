f325b4a7f45bcbd96d1ee4ddac43d135bdd0118e
AlrgabeAh2025 <alrgabeah.2025@gmail.com>
2025-02-27 15:50:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,103 @@
+from flet import *
+from views.login_screen.login_screen import Login
+from views.signup_screen.signup_screen import SignUp
+from views.welcome_screen.welcome_screen import Welcome
+from views.home_screen.home_screen import Home
+from views.profile_screen.profile_screen import (
+    Profile,
+    PersonalInformation,
+    SecurityPasswords,
+)
+import requests
+
+
+def main(page: Page):
+
+    page.fonts = {
+        "LateefBoldFont": "/fonts/Lateef,Rakkas/Lateef/Lateef-Bold.ttf",
+        "LateefNormalFont": "/fonts/Lateef,Rakkas/Lateef/Lateef-Medium.ttf",
+        "Rakkas": "/fonts/Lateef,Rakkas/Rakkas/Rakkas-Regular.ttf",
+        "ElMessiri": "/fonts/El_Messiri,Lateef,Rakkas/El_Messiri/ElMessiri-VariableFont_wght.ttf",
+    }
+
+    page.theme_mode = ThemeMode.LIGHT
+    page.rtl = True
+    page.client_storage.clear()
+    page.theme = Theme(
+        font_family="LateefNormalFont",
+        color_scheme_seed="#666666",
+        text_theme=TextStyle(color="#110b22", font_family="LateefBoldFont"),
+        appbar_theme=AppBarTheme(bgcolor="#110b22", color="#ffffff"),
+    )
+
+    baseUrl = "http://192.168.6.192:2010"
+
+    def showMessage(text):
+        snack_bar = SnackBar(
+                    content=Text(
+                        f"{text}",
+                        style=TextStyle(size=15, font_family="ElMessiri"),
+                    ),
+                    show_close_icon=True
+                )
+        page.open(snack_bar)
+    
+    def route_change(e):
+        routes = {
+            "/":Welcome,
+            "/home":Home,
+            "/login":Login,
+            "/signup":SignUp,
+            "/Profile":Profile,
+            "/PersonalInformation":PersonalInformation,
+            "/SecurityPasswords":SecurityPasswords,
+        }
+
+        # مسح الشاشة الحالية
+        page.views.clear()
+
+        # جلب الشاشة المناسبة بناءً على المسار
+        page_class = routes.get(page.route, None)
+        page_class.baseUrl = baseUrl
+        if page_class:
+            page.views.append(
+                page_class(route=page.route, page=page)
+            )  # إضافة الشاشة المناسبة
+        else:
+            page.views.append(Text("Page not found"))  # إذا لم يكن المسار موجودًا
+
+        page.update()
+
+    def refreshAccessToken():
+        if page.client_storage.contains_key("refresh"):
+            async def refresh(refresh_token):
+                body={"refresh": refresh_token}
+                try:
+                    response = requests.post(url=f"{baseUrl}/refresh/", data=body)
+                    json = response.json()
+                    if response.status_code == 200:
+                        return [True , json]
+                    else:
+                        return [False , "الرجاء التحقق من اتصال الانترنت"]
+                except requests.exceptions.Timeout:
+                    return [False , "الرجاء التحقق من اتصال الانترنت"]
+                except requests.exceptions.ConnectionError:
+                    return [False , "لم نتمكن من الوصول الى الخادم الرجاء اعادة المحاولة"]
+            refresh_token = page.client_storage.get("refresh")
+            result = page.run_task(refresh , refresh_token)
+            if result.result()[0]:
+                page.client_storage.set("access", result.result()[1]['access'])
+                page.client_storage.set("refresh",result.result()[1]['refresh'])  
+                return [result.result()[0] , result.result()[1]]
+            else :return [result.result()[0] , result.result()[1]]
+        else:
+            return [False , "سجل الدخول او انشئ حساب"]
+        
+    page.on_route_change = route_change
+    result = refreshAccessToken()
+    if result[0]:
+        page.go("/home")
+    else:
+        page.go("/")
+        showMessage(result[1])
+app(main, assets_dir="assets")
---
+++ views/home_screen/home_screen.py
@@ -0,0 +1,409 @@
+import qrcode, base64, requests
+from io import BytesIO
+from flet import *
+
+
+
+
+
+class Home(View):
+    def __init__(self, route, page):
+        super().__init__(route=route)
+        self.scroll = ScrollMode.HIDDEN
+        self.page = page
+
+        self.drawer = NavigationDrawer(
+            on_change=self.handle_change,
+            controls=[
+                Container(height=12),
+                NavigationDrawerDestination(
+                    label="الرئيسية",
+                    icon_content=Icon(icons.HOME_OUTLINED),
+                    selected_icon_content=Icon(icons.HOME),
+                ),
+                NavigationDrawerDestination(
+                    label="تسجيل الخروج",
+                    icon_content=Icon(icons.LOGOUT_OUTLINED),
+                    selected_icon_content=Icon(icons.LOGOUT),
+                ),
+            ],
+        )
+
+        self.appbar = AppBar(
+            actions=[
+                IconButton(
+                    icon=icons.PERSON,
+                    icon_color="#ffffff",
+                    on_click=lambda x: self.page.go("/Profile"),
+                ),
+            ],
+            leading=IconButton(
+                icon=icons.MENU,
+                icon_color="#ffffff",
+                on_click=lambda e: self.page.open(self.drawer),
+            ),
+            toolbar_height=100,
+            title=Text(
+                "حماية الاطفال",
+                size=20,
+                weight=FontWeight.BOLD,
+                color="#ffffff",
+                font_family="ElMessiri",
+            ),
+        )
+
+    def copy(self, data):
+        self.page.set_clipboard(data)
+        self.page.open(SnackBar(Text("تم نسخ المفتاح"), open=True))
+        self.page.update()
+
+    def hasNoFather(self):
+        self.scroll = ScrollMode.ALWAYS
+        userData = self.page.client_storage.get("userData")
+        qr = self.generateQr(userData)
+        self.controls.clear()
+        self.controls.append(
+            ResponsiveRow(
+                controls=[
+                    Container(
+                        content=Column(
+                            controls=[
+                                ListTile(
+                                    leading=Icon(icons.INFO),
+                                    title=Text(
+                                        "امسح رمز (QR) بالاسفل للاقتران بهاذة الجهاز",
+                                        size=13,
+                                        weight=FontWeight.NORMAL,
+                                        color="#666666",
+                                        font_family="ElMessiri",
+                                    ),
+                                ),
+                                Container(
+                                    content=Text(
+                                        "اولا يجب ان تقوم بإنشاء حساب في هاتف الاب ثم يمكنك مسح رمز (QR) بالاسفل للاقتران مع هذا الجهاز",
+                                        size=13,
+                                        weight=FontWeight.NORMAL,
+                                        color="#666666",
+                                        font_family="ElMessiri",
+                                    ),
+                                    padding=10,
+                                ),
+                            ]
+                        ),
+                        bgcolor="#ffffff",
+                        border=border.all(color="#110b22", width=1),
+                        border_radius=border_radius.all(10),
+                    ),
+                    Container(height=10),
+                    Container(
+                        content=Text(
+                            "ادخل هذا في هاتف الاب للأقتران بالجهاز",
+                            size=13,
+                            weight=FontWeight.NORMAL,
+                            color="#666666",
+                            font_family="ElMessiri",
+                        ),
+                    ),
+                    Container(
+                        content=ListTile(
+                            title=Text(
+                                f"{userData['key']}",
+                                style=TextStyle(
+                                    size=11,
+                                    weight=FontWeight.BOLD,
+                                    font_family="ElMessiri",
+                                ),
+                            ),
+                            trailing=IconButton(
+                                icon=icons.COPY,
+                            ),
+                        ),
+                        bgcolor="#ffffff",
+                        border=border.all(0.5, "#110b22"),
+                        border_radius=border_radius.all(5),
+                        on_click=lambda x: self.copy(f"{userData['key']}"),
+                    ),
+                    Container(height=10),
+                    Container(
+                        content=Text(
+                            "أو إمسح رمز (QR) للحصول على المفتاح",
+                            size=13,
+                            weight=FontWeight.NORMAL,
+                            color="#666666",
+                            font_family="ElMessiri",
+                        ),
+                    ),
+                    Container(
+                        content=Column(
+                            controls=[
+                                Image(src_base64=qr, width=200),
+                                Container(
+                                    content=Text(
+                                        "امسح رمز (QR)",
+                                        size=14,
+                                        weight=FontWeight.NORMAL,
+                                        color="#666666",
+                                        font_family="ElMessiri",
+                                    ),
+                                ),
+                            ],
+                            horizontal_alignment=CrossAxisAlignment.CENTER,
+                            alignment=MainAxisAlignment.CENTER,
+                        ),
+                        bgcolor="#ffffff",
+                        border=border.all(color="#110b22", width=1),
+                        border_radius=border_radius.all(10),
+                        padding=padding.symmetric(vertical=10),
+                    ),
+                ],
+                expand=True,
+            )
+        )
+        self.update()
+
+    def hasFatherScreen(self):
+        self.controls.clear()
+        self.controls.append(
+            ResponsiveRow(
+                controls=[
+                    Container(height=10),
+                    Column(
+                        controls=[
+                            Container(
+                                content=Image(src="images/father.png", width=150),
+                                # border_radius=border_radius.all(150),
+                            ),
+                            Container(
+                                content=Text(
+                                    "اسم الاب",
+                                    size=20,
+                                    weight=FontWeight.BOLD,
+                                    color="#666666",
+                                    font_family="ElMessiri",
+                                ),
+                            ),
+                            Container(height=20),
+                            ResponsiveRow(
+                                controls=[
+                                    Text(
+                                        "البيانات التي يتم مر اقبتها",
+                                        style=TextStyle(
+                                            size=12,
+                                            weight=FontWeight.BOLD,
+                                            font_family="ElMessiri",
+                                        ),
+                                        color="#666666",
+                                        text_align=TextAlign.START,
+                                    ),
+                                ],
+                            ),
+                            Container(
+                                content=ResponsiveRow(
+                                    controls=[
+                                        ListTile(
+                                            title=Text(
+                                                "مراقبة استخدام التطبيقات",
+                                                style=TextStyle(
+                                                    size=14,
+                                                    weight=FontWeight.BOLD,
+                                                    font_family="ElMessiri",
+                                                ),
+                                            ),
+                                            trailing=Switch(
+                                                value=False,
+                                                active_color="#110b22",
+                                                on_click=lambda x: self.startGetScreenShot()
+                                            ),
+                                            subtitle=Text(
+                                                "مغلق",
+                                                style=TextStyle(
+                                                    size=10,
+                                                    weight=FontWeight.BOLD,
+                                                    font_family="ElMessiri",
+                                                ),
+                                            ),
+                                        ),
+                                    ],
+                                ),
+                                bgcolor="#ffffff",
+                                border=border.all(0.5, "#110b22"),
+                                border_radius=border_radius.all(5),
+                                alignment=alignment.center,
+                                
+                            ),
+                            Container(
+                                content=ResponsiveRow(
+                                    controls=[
+                                        ListTile(
+                                            title=Text(
+                                                "مراقبة الشاشة",
+                                                style=TextStyle(
+                                                    size=14,
+                                                    weight=FontWeight.BOLD,
+                                                    font_family="ElMessiri",
+                                                ),
+                                            ),
+                                            trailing=Switch(
+                                                value=False,
+                                                active_color="#110b22",
+                                            ),
+                                            subtitle=Text(
+                                                "مغلق",
+                                                style=TextStyle(
+                                                    size=10,
+                                                    weight=FontWeight.BOLD,
+                                                    font_family="ElMessiri",
+                                                ),
+                                            ),
+                                        ),
+                                    ],
+                                ),
+                                bgcolor="#ffffff",
+                                border=border.all(0.5, "#110b22"),
+                                border_radius=border_radius.all(5),
+                                alignment=alignment.center,
+                            ),
+                            Container(height=20),
+                            ResponsiveRow(
+                                controls=[
+                                    Text(
+                                        "ايقاف عمل التطبيق",
+                                        style=TextStyle(
+                                            size=12,
+                                            weight=FontWeight.BOLD,
+                                            font_family="ElMessiri",
+                                        ),
+                                        color="#666666",
+                                        text_align=TextAlign.START,
+                                    ),
+                                ],
+                            ),
+                            ResponsiveRow(
+                                controls=[
+                                    Container(
+                                        content=ListTile(
+                                            title=Text(
+                                                "الغاء الربط مع هاتف الاب",
+                                                style=TextStyle(
+                                                    size=15,
+                                                    weight=FontWeight.BOLD,
+                                                    font_family="ElMessiri",
+                                                ),
+                                            ),
+                                            trailing=IconButton(
+                                                icon=icons.CANCEL,
+                                            ),
+                                        ),
+                                        bgcolor="#ffffff",
+                                        border=border.all(0.5, "#110b22"),
+                                        border_radius=border_radius.all(10),
+                                    ),
+                                ],
+                            ),
+                        ],
+                        horizontal_alignment=CrossAxisAlignment.CENTER,
+                        alignment=MainAxisAlignment.CENTER,
+                    ),
+                ],
+                expand=True,
+            )
+        )
+        self.update()
+
+    def did_mount(self):
+        self.loaderUi()
+        self.checkIsThereFather()
+
+    def generateQr(self, userData):
+        qrCode = qrcode.make(f"{userData['key']}")
+        buffered = BytesIO()
+
+        qrCode.save(buffered, format="PNG")
+        sl = base64.b64encode(buffered.getvalue())
+        resultOfQr = sl.decode("utf-8")
+        return resultOfQr
+
+    async def sendHeadRequest(self, url, body={}):
+        access = await self.page.client_storage.get_async("access")
+        headers = {
+            "Authorization": f"Bearer {access}",
+            "Accept": "*/*",
+            "Cache-Control": "no-cache",
+        }
+        try:
+            response = requests.head(
+                url=f"{Home.baseUrl}/{url}/", data=body, headers=headers
+            )
+            if response.status_code == 200:
+                return [True, "has"]
+            else:
+                return [False, "hasNo"]
+        except requests.exceptions.Timeout:
+            return [False, "اتصال الانترنت بطئ"]
+        except requests.exceptions.ConnectionError:
+            return [False, "حدث خطأ في الاتصال بالخادم. تحقق من اتصالك بالإنترنت."]
+    
+    async def getBackground(self):
+        try:
+            response = requests.get(
+                url=f"http://localhost:8080/screenshot"
+            )
+            if response.status_code == 200:
+                print(response)
+                return [True, response]
+            else:
+                return [False, "hasNo"]
+        except requests.exceptions.Timeout:
+            return [False, "اتصال الانترنت بطئ"]
+        except requests.exceptions.ConnectionError:
+            return [False, "حدث خطأ في الاتصال بالخادم. تحقق من اتصالك بالإنترنت."]
+
+    def showMessage(self, message):
+        snack_bar = SnackBar(
+            content=Text(
+                f"{message}",
+                style=TextStyle(size=15, font_family="ElMessiri"),
+            ),
+            show_close_icon=True,
+        )
+        self.page.open(snack_bar)
+
+    def startGetScreenShot(self):
+        state, result = self.page.run_task(self.getBackground).result()
+        print(state)
+        self.showMessage(result)
+
+    def loaderUi(self):
+        self.scroll = None
+        self.controls.clear()
+        self.controls.append(
+            Column(
+                controls=[
+                    Container(
+                        content=ProgressRing(visible=True),  # Progress ring loader
+                        alignment=alignment.center,
+                        height=float("inf"),  # Make the container take full height
+                        expand=True,  # Ensure the container expands to fill available space
+                    ),
+                ],
+                expand=True,  # Make the column expand to take up all available space
+            )
+        )
+        self.update()
+
+    def handle_change(self, e):
+        routs = {
+            "0": "/home",
+            "1": "/",
+        }
+        if routs[e.data] == "/":
+            self.page.client_storage.clear()
+        self.page.go(routs[e.data])
+        self.page.close(self.drawer)
+
+    def checkIsThereFather(self):
+        state, result = self.page.run_task(self.sendHeadRequest, "Children").result()
+        if not state:
+            self.hasNoFather()
+        else:
+            self.hasFatherScreen()
